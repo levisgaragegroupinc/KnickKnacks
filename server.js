ec5115e24613c21c5719bfffb53e594e82d37802
rajeswari marimuthu <rajeswarivmarimuthu@gmail.com>
2022-05-28 18:04:25 -0700

    committing service rout for sample query'

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 // import sequelize connection
 const sequelize = require('./config/connection');
 const app = express();
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//app.use(routes);
+app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
