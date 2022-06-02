f887c4b32831a575242f0527ffcfced7890e55bb
rajeswari marimuthu <rajeswarivmarimuthu@gmail.com>
2022-06-01 21:13:05 -0700

    commit user profile changes in home route

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -8,7 +8,7 @@ router.get('/', (req, res) => {
     res.render('homepage'),{
         logged_in: req.session.logged_in
     };
-  });
+});
 
 // Setting up routes for user login that would render login page
 router.get('/login', (req, res) => {
@@ -17,7 +17,7 @@ router.get('/login', (req, res) => {
       return;
     }
     res.render('login');
-  });
+});
 
 //Setting up router for signup page that would render signup page
 router.get('/signup', (req, res) => {
@@ -26,15 +26,26 @@ router.get('/signup', (req, res) => {
       return;
     }
     res.render('signup');
-  });
+});
 
 
-  //Setting up router for search results 
-  router.get('/search-results', withAuth, (req,res)=> {
-      
-    res.render('search-results', { 
-        providers, 
+//Setting up router for search results 
+router.get('/search-results', withAuth, (req,res)=> {
+
+res.render('search-results', { 
+    providers, 
+    logged_in: req.session.logged_in
+    });
+
+});
+
+
+//Setting up router for search results 
+router.get('/:username', withAuth, (req,res)=> {
+    
+    res.render('userprofile', { 
+        user_profile, 
         logged_in: req.session.logged_in
-      });
+    });
 
-  });
+});
