3c1d53af127c395a8d66d7b757acf673718ba354
Phoenix Staley <PhoenixStaley121@hotmail.com>
2022-06-04 13:21:18 -0700

    Add POST and DELETE routes

--- controllers/api/skill-routes.js
+++ controllers/api/skill-routes.js
@@ -1,8 +1,42 @@
 const router = require('express').Router();
 const { ProviderServiceArea , ProviderSkill, ServiceArea, Skill, User} = require('../../models');
 
-router.get("/:username", (req, res) => {
-    
+router.post("/:username", async (req, res) => {
+    try {
+        const user = await User.findOne({ where: { username: req.params.username } });
+        const existing_skill = await Skill.findOne({ where: { skill_name: req.body.skill }});
+        let new_skill;
+        if (!existing_skill) {
+            new_skill = await Skill.create({ skill_name: req.body.skill });
+        } else {
+            new_skill = existing_skill;
+        }
+        await ProviderSkill.create({
+            skill_id: new_skill.id,
+            user_id: user.id
+        });
+
+        res.status(201).json({ message: `${req.body.skill} added as a new skill for ${req.params.username}!` });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.delete("/:username", async (req, res) => {
+    try {
+        const user = await User.findOne({ where: { username: req.params.username } });
+        const skill = await Skill.findOne({ where: {
+            skill: req.body.skill
+        }});
+        await ProviderSkill.destroy({ where: {
+            skill_id: skill.id,
+            user_id: user.id
+        }});
+
+        res.status(201).json({ message: `${req.body.skill} deleted from ${req.params.username}!` });
+    } catch (err) {
+        res.status(400).json(err);
+    }
 });
 
 module.exports = router;
