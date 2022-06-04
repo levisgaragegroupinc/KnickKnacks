75f511f8616943acb9cff89b496ff85827fc1d9d
rajeswari marimuthu <rajeswarivmarimuthu@gmail.com>
2022-06-04 11:44:35 -0700

    Committing send-emil request:

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const service_routes = require('./service_route');
+const req_service = require ('./request_service_route');
 
 router.use('/services', service_routes);
+router.use('/request-service', req_service)
 module.exports = router;
