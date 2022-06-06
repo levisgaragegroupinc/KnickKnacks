8dd1b2c98cb98275a39aa29098760394d8667284
Phoenix Staley <PhoenixStaley121@hotmail.com>
2022-06-06 11:15:30 -0700

    Add request-routes to api/index.js

---
+++ controllers/api/request-routes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const { ProviderServiceArea , ProviderSkill, ServiceArea, Skill, User} = require('../../models');
+
+router.get("/:username", async (req, res) => {
+    
+});
+
+module.exports = router;
