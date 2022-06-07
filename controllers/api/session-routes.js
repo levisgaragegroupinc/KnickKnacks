57a621c00b4c860e7e059bd1aabc8c55d0daf662
Phoenix Staley <PhoenixStaley121@hotmail.com>
2022-06-07 10:47:42 -0700

    Fix dispairty in route names

--- controllers/api/session-routes.js
+++ controllers/api/session-routes.js
@@ -33,7 +33,7 @@ router.post("/login", async (req, res) => {
     }
 });
 
-router.post("/logout", (req, res) => {
+router.post("/log_out", (req, res) => {
     if (req.session.logged_in) {
         req.session.destroy(() => {
             res.status(204).end();
