ed0ff33160a331b38a8f5f3de21891808901adcb
Phoenix Staley <PhoenixStaley121@hotmail.com>
2022-06-06 19:43:10 -0700

    Remove request-routes.js to avoid merge conflict

--- controllers/api/request-routes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const { ProviderServiceArea , ProviderSkill, ServiceArea, Skill, User} = require('../../models');
-
-router.get("/:username", async (req, res) => {
-    
-});
-
-module.exports = router;
