227ed732811a2f9655e1668510b8170a7483932b
Phoenix Staley <PhoenixStaley121@hotmail.com>
2022-06-07 10:30:50 -0700

    Re-add route that was commented out

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,10 +2,10 @@ const router = require("express").Router();
 
 const session_routes = require("./session-routes");
 const skill_routes = require("./skill-routes");
-// const request_routes = require("./request-routes");
+const request_routes = require("./request-routes");
 
 router.use("/sessions", session_routes);
 router.use("/skill", skill_routes);
-// router.use("/request", request_routes);
+router.use("/request", request_routes);
 
 module.exports = router;
