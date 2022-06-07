0b5d3d8b107bbc5dc3baaf7924a5ce2a47b7de6b
rajeswari marimuthu <rajeswarivmarimuthu@gmail.com>
2022-06-06 19:27:49 -0700

    committing emailing code

--- controllers/api/request_service_route.js
+++ controllers/api/request_service_route.js
@@ -1,28 +1,44 @@
 const router = require('express').Router();
 const { request } = require('express');
 const { ProviderServiceArea , ProviderSkill, ServiceArea, Skill, User} = require('../../models');
-const axios = require('axios')
+const axios = require('axios');
+const { restore } = require('../../models/User'); 
+// const mailchimpClient = require("@mailchimp/mailchimp_transactional")("MQ0pLk82xHWK2bgI_D7JNQ");
 
-console.log (' In request-service-route');
 
-const mc_instance='us11'
-const mc_listUniqueId='c5327bf863'  
-const mc_ApiKey='019a34459eda1ef79f911e4e8eabb20c-us11'
+// console.log (' In request-service-route');
 
-const add_users_url = 'https://' + mc_instance + '.api.mailchimp.com/3.0/lists/' + mc_listUniqueId + '/members/'
-// const mc_auth_header = `Bearer ${mc_ApiKey}`
+// const mc_instance='us11'
+// const mc_listUniqueId='c5327bf863'  
+// const mc_ApiKey='019a34459eda1ef79f911e4e8eabb20c-us11'
 
-router.post('/addemails', async (req, res) => {
-    try {
-        const resp = await axios.post(
-            add_users_url,
-            req.body,
-            { headers: { Authorization: `Bearer ${mc_ApiKey}` } }
-        )
-        res.status(200).json(resp.data);
-      } catch (error) {
-        console.log(`error`, error)
-      }
-});
+// const add_users_url = 'https://' + mc_instance + '.api.mailchimp.com/3.0/lists/' + mc_listUniqueId + '/members/'
+// // const mc_auth_header = `Bearer ${mc_ApiKey}`
+
+// router.post('/addemails', async (req, res) => {
+//     try {
+//         const resp = await axios.post(
+//             add_users_url,
+//             req.body,
+//             { headers: { Authorization: `Bearer ${mc_ApiKey}` } }
+//         )
+//         res.status(200).json(resp.data);
+//       } catch (error) {
+//         console.log(`error`, error)
+//       }
+// });
+
+
+// router.post('/sendrequest', async(req,res)=>{
+//     try{
+//     const response = await mailchimpClient.messages.send({ message: req.body});
+//     console.log(response);
+//     res.status(200).json(response.data);
+//     }
+//     catch (err) {
+//         res.status(500).json(response.data);
+//     }
+// })
+
 
 module.exports = router;
