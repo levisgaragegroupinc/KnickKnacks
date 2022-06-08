1877cd0960dd2bc2e350f6e5e1c9940fe0c16003
Travis Bates <tbateswsu@gmail.com>
2022-06-07 20:34:21 -0700

    bug: request-routes missing s

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 const session_routes = require("./session-routes");
 const skill_routes = require("./skill-routes");
-const request_routes = require("./request-route");
+const request_routes = require("./request-routes");
 
 router.use("/sessions", session_routes);
 router.use("/skill", skill_routes);
