3229908235c8ed33b1ecef6852f2b759bf2c66ed
Travis Levi B <tbateswsu@gmail.com>
2022-06-08 16:26:08 -0700

    Refactor: cleaning up html, and fixing styling on profile page when the bio has not content.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,86 +1,94 @@
 // importing necessary package
 const router = require("express").Router();
-const { User, ServiceArea, Skill, ProviderSkill, ProviderServiceArea } = require("../models");
+const {
+  User,
+  ServiceArea,
+  Skill,
+  ProviderSkill,
+  ProviderServiceArea,
+} = require("../models");
 const withAuth = require("../utils/withAuth");
 
 //Setting up router for rendering homepage
 router.get("/", (req, res) => {
-    res.render("home", {
-        logged_in: req.session.logged_in
-    });
+  res.render("home", {
+    logged_in: req.session.logged_in,
+  });
 });
 
 // Setting up routes for user login that would render login page
 router.get("/login", (req, res) => {
-    if (req.session.logged_in) {
-      res.redirect("/");
-      return;
-    }
-    res.render("login", {
-        logged_in: req.session.logged_in
-    });
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+  res.render("login", {
+    logged_in: req.session.logged_in,
+  });
 });
 
 //Setting up router for signup page that would render signup page
 router.get("/signup", (req, res) => {
-    if (req.session.logged_in) {
-      res.redirect("/");
-      return;
-    }
-    res.render("signup", {
-        logged_in: req.session.logged_in
-    });
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+  res.render("signup", {
+    logged_in: req.session.logged_in,
+  });
 });
 
 router.get("/favicon.ico", (req, res) => {
-    res.status(404);
+  res.status(404);
 });
 
-//Setting up router for search results 
-router.get("/:username", withAuth, async (req, res)=> {
-    const user_data = await User.findOne({
-        where: {
-            username: req.params.username
-        },
-        include: {
-            model: Skill,
-            as: "providers_skills"
-        }
-    });
-    const user = user_data.get({ plain: true });
-    
-    res.render("home-profile", { 
-        user,
-        logged_in: req.session.logged_in,
-        editting: false
-    });
+//Setting up router for search results
+router.get("/:username", withAuth, async (req, res) => {
+  const user_data = await User.findOne({
+    where: {
+      username: req.params.username,
+    },
+    include: {
+      model: Skill,
+      as: "providers_skills",
+    },
+  });
 
+  console.log("This is the user_data on the home-routes", user_data);
+  const user = user_data.get({ plain: true });
+
+  res.render("home-profile", {
+    user,
+    logged_in: req.session.logged_in,
+    editting: false,
+  });
 });
 
 // Add route to edit someone's profile
-router.get("/:username/edit", withAuth, async (req,res)=> {
-    const user = await User.findOne({
-        where: {
-            username: req.params.username
-        }
-    });
-    
-    res.render("home-profile", { 
-        user,
-        logged_in: req.session.logged_in,
-        editting: true
-    });
+router.get("/:username/edit", withAuth, async (req, res) => {
+  const user = await User.findOne({
+    where: {
+      username: req.params.username,
+    },
+  });
 
+  res.render("home-profile", {
+    user,
+    logged_in: req.session.logged_in,
+    editting: true,
+  });
 });
 
 // Renders the form to request services from [username]
 router.get("/request/:username", async (req, res) => {
-    const provider = await User.findOne({ where: { username: req.params.username } });
+  const provider = await User.findOne({
+    where: { username: req.params.username },
+  });
 
-    res.render("request-service-form", {
-        logged_in: req.session.logged_in,
-        bio: provider.provider_bio
-    });
+  res.render("request-service-form", {
+    logged_in: req.session.logged_in,
+    bio: provider.provider_bio,
+  });
 });
 
 module.exports = router;
