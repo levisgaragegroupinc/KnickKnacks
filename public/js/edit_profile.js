2476fb5c841666ae3ec57d712be8e57cf29c6c64
Cristobal Marquez-Glynn <cristobalmqz@gmail.com>
2022-06-08 21:08:25 -0700

    updated search.js and continue to work on edit_profile.js

--- public/js/edit_profile.js
+++ public/js/edit_profile.js
@@ -2,26 +2,28 @@
 const update_bio = async (event) => {
     event.preventDefault();
   
-    const textarea_bio = document.querySelector('#bio-text');
-    const description = textarea_bio.value.trim();
-    const username = textarea_bio.getAttribute('data-id')
+    const textarea_bio = document.querySelector('#bio-text').value.trim();
+    const username_data = document.querySelector('.profile-container-white') ;
+    const username = username_data.getAttribute('data-id');
   
-    if (title && description) {
-      const response = await fetch(`/api/bio/${username}`, {
-        method: 'POST',
-        body: JSON.stringify({ description }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-//   test
-      if (response.ok) {
-        document.location.reload();
-      } else {
-        alert('Failed to create project');
-      }
-    }
-  };
+    console.log(textarea_bio);
+    console.log(username);
+    
+    // const response = await fetch(`/api/bio/${username}`, {
+    //   method: 'POST',
+    //   body: JSON.stringify({ textarea_bio }),
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    // });
+
+    // if (response.ok) {
+    //   document.location.reload();
+    // } else {
+    //   alert('Failed to create project');
+    // }
+};
+
   //  fetch function to delete a post
   const delButtonHandler = async (event) => {
     if (event.target.hasAttribute('data-id')) {
