e04e4ecee71ca8fe500e5b7f423d3f21bbfc258f
rajeswari marimuthu <rajeswarivmarimuthu@gmail.com>
2022-06-09 20:42:24 -0700

    fixed the bugs in request.service to handle event

--- public/js/request-service.js
+++ public/js/request-service.js
@@ -1,45 +1,37 @@
-// JS module to handle route the request for service 
-
-const requestSvcHandler = async (event) => {
+//Send notificaton module
+req_notification_handler = async(event) => {
     event.preventDefault();
-    document.getElementById('req-container').style.display = "block";
-};
+    var req_elem = event.target;
+    var provider_username;
 
-
-const requestNotesHandler = async (event) => {
-    event.preventDefault();
-
-    //variables to retrieve provider_username and requestor notes
-    const request_notes = document.getElementById('request-text');
-    const provider_username = document.getElementById('prov-username');
-
-    //Post to send email notification 
-    if (request_notes) {
+//enabling request notes section
+    if (event.target.id === 'req-svc-btn') {
+        provider_username = req_elem.parentElement.children[0].textContent;
+        req_elem.parentElement.nextElementSibling.style.display = "block";
+    }
+//upon send request, email router is called
+    else if(event.target.id === 'req-notes-btn') {
+        const requestor_notes = req_elem.parentElement.children[1].children[0].value.trim();
+        if (requestor_notes) {
+    
         const response = await fetch('/api/request/sendemail', {
             method: 'POST',
-            body: JSON.stringify({ provider_username, request_notes }),
+            body: JSON.stringify({ provider_username, requestor_notes}),
             headers: { 'Content-Type': 'application/json' },
         });
-
+    
         if (response.ok) {
-            console.log(response);
+    
             alert('Provider is notifed and will be in touch with you soon');
-            document.getElementById('req-container').style.display = "none";
-            document.getElementById('request-text').value = '';
+            req_elem.parentElement.children[1].children[0].value = '';
+            req_elem.parentElement.style.display = "none";
+            return;
         } else {
             alert(response.statusText);
         }
-
-    } else {
+        } else {
         alert ('request notes can not be empty');
-    };
-};
-
-
-document
-.querySelector('#req-svc-btn')
-.addEventListener('click', requestSvcHandler);
-
-document
-.querySelector('#req-notes-btn')
-.addEventListener('click', requestNotesHandler);
+        };
+    }
+    else {return};
+}
