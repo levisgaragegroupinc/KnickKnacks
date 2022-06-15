fa8ef5da49877297890c3b1cd2cb3793f3497668
Phoenix Staley <PhoenixStaley121@hotmail.com>
2022-06-15 11:05:11 -0700

    Move repeated code into a function

--- controllers/api/skill-routes.js
+++ controllers/api/skill-routes.js
@@ -1,44 +1,51 @@
 const router = require('express').Router();
 const { ProviderServiceArea , ProviderSkill, ServiceArea, Skill, User} = require('../../models');
 
+const record_skill = async (skill_id, user, req)  => {
+    const existing_record = await ProviderSkill.findOne({
+        where: {
+            skill_id: skill_id,
+            user_id: user.id
+        }
+    });
+    if (existing_record) {
+        res.status(409).json({ message: `${req.params.username} is already associated with ${req.body.skill}` });
+        return;
+    } 
+    await ProviderSkill.create({
+        skill_id: skill_id,
+        user_id: user.id
+    });
+    
+    // user.dataValues.provider = true;
+    await user.update({provider: true});
+}
+
 router.post("/:username", async (req, res) => {
     try {
         const user = await User.findOne({ where: { username: req.params.username }});
         if (!user) {
             res.status(404).json({ message: "There is no user with this username"});
             return;
-        } console.log('line 10: '+req.body.skill);
+        }
         if (!req.body.skill) {
             res.status(400).json({ message: "Request body must contain a skill property"});
         }
         const existing_skill_data = await Skill.findOne({ where: { skill_name: req.body.skill }});
-        const existing_skill = existing_skill_data.get({ plain: true });
+        let existing_skill;
+        if (existing_skill_data) {
+            existing_skill = existing_skill_data.get({ plain: true });
+        }
         let new_skill;
        
-        if (!existing_skill.skill_name) {
+        if (!existing_skill?.skill_name) {
             new_skill = await Skill.create({ skill_name: req.body.skill });
+            record_skill(new_skill.id, user, req);
+            res.status(201).json({ message: `${req.body.skill} added as a new skill for ${req.params.username}!` });
         } else {
             new_skill = existing_skill.skill_name;
             new_skill_id = existing_skill.id;
-            // console.log('***line 29 - NewSKILL: '+new_skill + ', skill id '+new_skill_id +', user.id: '+ user.id)
-            const existing_record = await ProviderSkill.findOne({
-                where: {
-                    skill_id: new_skill_id,
-                    user_id: user.id
-                }
-            });
-            // console.log("Existing record:", existing_record);
-            if (existing_record) {
-                res.status(409).json({ message: `${req.params.username} is already associated with ${req.body.skill}` });
-                return;
-            } 
-            await ProviderSkill.create({
-                skill_id: new_skill_id,
-                user_id: user.id
-            });
-            
-            // user.dataValues.provider = true;
-            await user.update({provider: true});
+            await record_skill(new_skill_id, user, req);
     
             res.status(201).json({ message: `${req.body.skill} added as a new skill for ${req.params.username}!` });
         }
